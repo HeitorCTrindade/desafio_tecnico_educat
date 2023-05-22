69842fcf1f6744a347a5a23d1ab1a1ac402f75b2
HeitorCTrindade <heitor-trindade@hotmail.com>
2023-05-22 10:03:41 -0300

    feat: finaliza implementacao dos testes para o desafio2

--- tests/desafio02.test.ts
+++ tests/desafio02.test.ts
@@ -1,11 +1,11 @@
-import formatStr from '../functions/desafio01';
+import formatStr from '../functions/desafio02';
 
 describe('1 - Testa função formatStr', () => {
-	it(`Será validado se fução retorna corretamente a string com as vogais trocadas por - e todos os demais caracteres maiúsculos`, async () => {
-		expect(formatStr([68, 76, 97, 56, 90])).toEqual([68, 76, 56, 90]);
+	it(`Será validado se função retorna corretamente a string "Hello World" com as vogais trocadas por '-' e todos os demais caracteres maiúsculos`, async () => {
+		expect(formatStr('Hello World')).toMatch('H-LL- W-RLD');
 	});
 
-	it('Será validado se fução retorna um array vazio quando não encontra numeros pares no array', async () => {
-		expect(findEvens([55, 67, 201])).toEqual([]);
+	it(`Será validado se função retorna corretamente a string "Estagio eduCAT" com as vogais trocadas por '-' e todos os demais caracteres maiúsculos`, async () => {
+		expect(formatStr('Estagio eduCAT')).toMatch('-ST-G-- -D-C-T');
 	});
 });

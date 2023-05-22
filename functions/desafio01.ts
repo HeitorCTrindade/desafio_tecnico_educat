f8cac24f16aee57cfbbcffa088fcedfadbe8e1ef
HeitorCTrindade <heitor-trindade@hotmail.com>
2023-05-22 09:36:48 -0300

    feat: cria funcao e teste simples para verificar se o projeto esta funcionando corretamente

--- functions/desafio01.ts
+++ functions/desafio01.ts
@@ -1,1 +1,9 @@
 // 01
+
+const isOdd = (n: number): boolean => {
+	return Boolean(n % 2);
+};
+
+console.log(isOdd(5));
+
+export default isOdd;
--- tests/desafio01.ts
+++ tests/desafio01.ts
@@ -1,1 +1,9 @@
 // test 01
+
+import isOdd from '../functions/desafio01';
+
+describe('1 - Testa se um numero é impar', () => {
+	it('Será validado se fução retorna TRUE quando um número impar é informado', async () => {
+		expect(isOdd(5)).toBe(true);
+	});
+});

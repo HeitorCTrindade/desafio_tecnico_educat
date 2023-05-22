c81c02f83f387a7f1c415834cd1fd9c038557190
HeitorCTrindade <heitor-trindade@hotmail.com>
2023-05-22 09:49:14 -0300

    feat: implementa o primeiro desafio proposto e seus testes

--- functions/desafio01.ts
+++ functions/desafio01.ts
@@ -1,9 +1,15 @@
-// 01
+// - Escreva uma função que receba uma lista de números inteiros como parâmetro e retorne uma nova lista contendo apenas os números pares da lista original.
+
+const findEvens = (list: number[]): number[] => {
+	const listEvens: number[] = [];
+	list.forEach((number) => {
+		if (!isOdd(number)) listEvens.push(number);
+	});
+	return listEvens;
+};
 
 const isOdd = (n: number): boolean => {
 	return Boolean(n % 2);
 };
 
-console.log(isOdd(5));
-
-export default isOdd;
+export default findEvens;
--- tests/desafio01.test.ts
+++ tests/desafio01.test.ts
@@ -1,9 +1,13 @@
 // test 01
 
-import isOdd from '../functions/desafio01';
+import findEvens from '../functions/desafio01';
 
-describe('1 - Testa se um numero é impar', () => {
-	it('Será validado se fução retorna TRUE quando um número impar é informado', async () => {
-		expect(isOdd(5)).toBe(true);
+describe('1 - Testa função findEvens', () => {
+	it('Será validado se fução retorna corretamente os numeros pares do array [68, 76, 97, 56, 90]', async () => {
+		expect(findEvens([68, 76, 97, 56, 90])).toEqual([68, 76, 56, 90]);
+	});
+
+	it('Será validado se fução retorna um array vazio quando não encontra numeros pares no array', async () => {
+		expect(findEvens([55, 67, 201])).toEqual([]);
 	});
 });

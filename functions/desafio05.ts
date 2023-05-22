93cc84a2bea034e5b263cdc08a3de4d7a418c7e6
HeitorCTrindade <heitor-trindade@hotmail.com>
2023-05-22 13:47:43 -0300

    refactor: refatora funcao isPrime para que tenha o funcionamento correto

--- functions/desafio05.ts
+++ functions/desafio05.ts
@@ -7,7 +7,7 @@ const verifyPrimes = (num: number): object => {
 const isPrime = (num: number): boolean => {
 	if (num < 2) return false;
 
-	for (let i = 2; i < Math.sqrt(num); i++) {
+	for (let i = 2; i <= Math.sqrt(num); i++) {
 		if (num % i === 0) {
 			return false;
 		}

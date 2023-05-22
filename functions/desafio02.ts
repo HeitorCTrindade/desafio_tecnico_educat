df40fcd02fd17a64599cc4385e44b2d64f16d91e
HeitorCTrindade <heitor-trindade@hotmail.com>
2023-05-22 10:11:31 -0300

    feat: implementa funcao formatStr

--- functions/desafio02.ts
+++ functions/desafio02.ts
@@ -1,7 +1,16 @@
 // - Escreva uma função que receba uma string como parâmetro e retorne uma nova string com todas as vogais substituídas pelo caractere "-" e todas as consoantes convertidas para maiúsculas. Por exemplo, se a string for "Hello World", a função deve retornar "H-LL- W-RLD".
 
 const formatStr = (str: string): string => {
-	return str;
+	let newStr = '';
+	for (let i = 0; i < str.length; i++) {
+		if (/^[aeiou]$/i.test(str[i])) {
+			newStr += '-';
+		} else {
+			newStr += str[i].toLocaleUpperCase;
+		}
+	}
+
+	return newStr;
 };
 
 export default formatStr;

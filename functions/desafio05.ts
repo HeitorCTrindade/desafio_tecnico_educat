4c3fc7113fc7520bfe86862c4cee97339edadc66
HeitorCTrindade <heitor-trindade@hotmail.com>
2023-05-22 13:16:38 -0300

    feat: cria assinatura das funcoes isPrime e findPrimesLessThan2000 para o desafio05

---
+++ functions/desafio05.ts
@@ -0,0 +1,12 @@
+// - Escreva uma função que receba um número inteiro como parâmetro e verifique se ele é um número primo. A função deve retornar True se o número for primo e False caso contrário. E deve identificar os números primos entre 0 e 2000.
+
+const isPrime = (num: number): boolean => {
+	return true;
+};
+
+const findPrimesLessThan2000 = (): number[] => {
+	const primes: number[] = [];
+	return primes;
+};
+
+export default isPrime;

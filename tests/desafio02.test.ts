d0dc1d52809a751dfae383195aae2bba0e445e8c
HeitorCTrindade <heitor-trindade@hotmail.com>
2023-05-22 09:57:57 -0300

    chore: libera a utilizacao de TemplateLiterals no eslint

--- tests/desafio02.test.ts
+++ tests/desafio02.test.ts
@@ -1,7 +1,7 @@
 import formatStr from '../functions/desafio01';
 
 describe('1 - Testa função formatStr', () => {
-	it('Será validado se fução retorna corretamente a string com as vogais trocadas por - e todos os demais caracteres maiúsculos', async () => {
+	it(`Será validado se fução retorna corretamente a string com as vogais trocadas por - e todos os demais caracteres maiúsculos`, async () => {
 		expect(formatStr([68, 76, 97, 56, 90])).toEqual([68, 76, 56, 90]);
 	});
 
